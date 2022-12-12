9fe92f85f1ca8918fb02fed68288867c6c97d8d2
m0rp3x <yurash612@gmail.com>
2022-12-12 22:08:41 +0300

    Initial commit

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApp21
+
+{
+
+    internal static class ZalupaHumana
+    {
+        public static void Main(string[] args)
+        {
+            
+            string connectionString = @"Data Source = 46.39.232.190; Initial Catalog = Airport_Nuclear_Bomb;User Id = m0rp3x228; Password = vag!nA228##
+;";
+            string sqlExpression = "SELECT COUNT(*) FROM Flight_Data";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                object count = command.ExecuteScalar();
+ 
+                command.CommandText = "SELECT MIN(Terminal_ID) FROM Gates";
+                object minAge = command.ExecuteScalar();
+ 
+                Console.WriteLine("В таблице {0} объектов", count);
+                Console.WriteLine("Минимальное значение: {0}", minAge);
+            }
+        }
+
+    }
+}
